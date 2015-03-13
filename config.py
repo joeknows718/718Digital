0d33537241129165993a9c0c6b4ba78a951d1572
Joe C <joeknows718@Joes-MacBook-Pro.local>
2015-03-12 23:05:38 -0400

    forms front end setup'

--- config.py
+++ config.py
@@ -1,5 +1,17 @@
 from key import key
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+SQLALCHEMY_DATABASEURI = 'sqlite:///' + os.path.join(basedir, app.db)
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 
 WTF_CSRF_ENABLED = True 
 SECRET_KEY = key
 
+OPENID_PROVIDERS = [
+	{'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id'},
+    {'name': 'Yahoo', 'url': 'https://me.yahoo.com'},
+    {'name': 'AOL', 'url': 'http://openid.aol.com/<username>'},
+    {'name': 'Flickr', 'url': 'http://www.flickr.com/<username>'},
+    {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}]
