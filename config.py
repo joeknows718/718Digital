94c430b7f4fe16cfe4ee8895082dd013a4d93ebf
JoeC718 <joe.carrano@colorofchange.org>
2015-03-13 15:02:47 -0400

    set up db and wrote up the scripts to create, migrate, and update/revert db

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-SQLALCHEMY_DATABASEURI = 'sqlite:///' + os.path.join(basedir, app.db)
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 
 WTF_CSRF_ENABLED = True 
